a98252fb907a1bd4678978a6947481533383f800
Yaateh Richardson <yaatehr@mit.edu>
2018-11-20 10:33:03 -0500

    another

--- train.py
+++ train.py
@@ -23,6 +23,23 @@ datestr = d.time().strftime("%H:%M:%S")
 
 cwd = os.getcwd() 
 
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
 def run():
     # Parameters
     num_epochs = 10
@@ -48,12 +65,16 @@ def run():
 
         while epoch <= num_epochs:
             running_loss = 0.0
+            epoch_train_loss = 0.0
+            epoch_val_loss = 0.0
+            correctInTrainEpoch = 0
+            top5InTrainEpoch = 0
+            epoch_samples = 0
+
             for param_group in optimizer.param_groups:
                 print('Current learning rate: ' + str(param_group['lr']))
             model.train()
 
-            correctInTrainEpoch = 0
-            top5InTrainEpoch = 0
             for batch_num, (inputs, labels) in enumerate(train_loader, 1):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
@@ -66,24 +87,64 @@ def run():
                 optimizer.step()
                 running_loss += loss.item()
 
-                with torch.no_grad():
-                    _, prediction = torch.max(outputs, dim=1)
-                    numCorrect = prediction == labels
-                    correctInTrainEpoch += sum(numCorrect)
-                    top5 = torch.topk(outputs, 5, dim=1)
-                    top5 = top5[:][1]
-                    accuracy = [1 if int(labels[ind]) in x else 0 for ind, x in enumerate(top5.numpy())]
-                    top5InTrainEpoch += sum(accuracy)
+                # with torch.no_grad():
+                #     _, prediction = torch.max(outputs, dim=1)
+                #     prediction = prediction.cpu()
+                #     outputs = outputs.cpu()
+                #     labels = labels.cpu()
+                #     numCorrect = prediction.numpy() == labels.numpy()
+                #     correctInTrainEpoch += sum(numCorrect)
+                #     top5 = torch.topk(outputs, 5, dim=1)
+                #     top5 = top5[:][1]
+                #     accuracy = [1 if int(labels[ind]) in x else 0 for ind, x in enumerate(top5.numpy())]
+                #     top5InTrainEpoch += sum(accuracy)
 
+                # if batch_num % output_period == 0:
+                #     print('[%d:%.2f] loss: %.3f' % (
+                #         epoch, batch_num*1.0/num_train_batches,
+                #         running_loss/output_period
+                #         ))
+                #     epoch_train_loss += running_loss
+                #     running_loss = 0.0
+                #     gc.collect()
+                # if batch_num > 5:
+                #     print('[%d:%.2f] loss: %.3f' % (
+                #         epoch, batch_num*1.0/num_train_batches,
+                #         running_loss/output_period
+                #         ))
+                #     epoch_train_loss += running_loss
+                #     running_loss = 0.0
+                #     gc.collect()
+                #     print(outputs)
+                #     _, prediction = torch.max(outputs, dim=1)
+                #     print('predicted class: ', prediction)
+                #     print('actual class: ', labels)
+                #     numCorrect = prediction == labels
+                #     print('classification error: ', sum(numCorrect)/len(labels))
+                #     top5 = torch.topk(outputs, 5, dim=1)
+                #     print('Top 5 classes were: ', top5[:][1])
+                #     top5 = top5[:][1]
+                #     # print(labels.repeat(1,5).view(5,-1))
+                #     print(labels.transpose(0,-1))
+                #     accuracy = [1 if int(labels[ind]) in x else 0 for ind, x in enumerate(top5.numpy())]
+                #     print('top5 accuracy: ', sum(accuracy)/len(labels))
+                #     break
+                acc1, acc5 = accuracy(outputs, labels, topk=(1,5))
+                n = outputs.size(0)
+                correctInTrainEpoch += acc1[0]*n
+                top5InTrainEpoch += acc5[0]*n
+                epoch_samples += n
                 if batch_num % output_period == 0:
                     print('[%d:%.2f] loss: %.3f' % (
                         epoch, batch_num*1.0/num_train_batches,
                         running_loss/output_period
                         ))
+                    epoch_train_loss += running_loss
                     running_loss = 0.0
                     gc.collect()
+                
                 # if batch_num > 5:
-                    # break
+                #     break
 
             gc.collect()
             # save after every epoch
@@ -92,23 +153,12 @@ def run():
             # TODO: Calculate classification error and Top-5 Error
             # on training and validation datasets here
 
-            # print(outputs)
-            # _, prediction = torch.max(outputs, dim=1)
-            # print('predicted class: ', prediction)
-            # numCorrect = prediction == labels
-            # print('classification error: ', sum(numCorrect)/len(labels))
-            # top5 = torch.topk(outputs, 5, dim=1)
-            # print('Top 5 classes were: ', top5[:][1])
-            # top5 = top5[:][1]
-            # # print(labels.repeat(1,5).view(5,-1))
-            # print(labels.transpose(0,-1))
-            # accuracy = [1 if int(labels[ind]) in x else 0 for ind, x in enumerate(top5.numpy())]
-            # print('top5 accuracy: ', sum(accuracy)/len(labels))
 
 
 
             correctInValEpoch = 0
             top5InValEpoch = 0
+
             model.eval()
             with torch.no_grad():
                 for batch_num, (inputs, labels) in enumerate(val_loader, 1):
@@ -116,22 +166,62 @@ def run():
                     labels = labels.to(device)
 
                     outputs = model(inputs)
-                    _, prediction = torch.max(outputs, dim=1)
-                    numCorrect = prediction == labels
-                    correctInValEpoch += sum(numCorrect)
-                    top5 = torch.topk(outputs, 5, dim=1)
-                    top5 = top5[:][1]
-                    accuracy = [1 if int(labels[ind]) in x else 0 for ind, x in enumerate(top5.numpy())]
-                    top5InValEpoch += sum(accuracy)
-                    # if batch_num > 5:
-                        # break
+                    loss = criterion(outputs, labels)
 
-            accuractyString = 'Epoch %d: T1  %.2f, T5 %.2f, V1 %.2f, V5 %.2f' % (
+                    acc1, acc5 = accuracy(outputs, labels, topk=(1,5))
+                    n = outputs.size(0)
+
+                    correctInValEpoch += acc1[0]*n
+                    top5InValEpoch += acc5[0]*n
+                    epoch_val_loss += loss.item()
+
+                    # if batch_num > 5:
+                    #     break
+                    
+
+                    # _, prediction = torch.max(outputs, dim=1)
+                    # prediction = prediction.cpu()
+                    # outputs = outputs.cpu()
+                    # labels = labels.cpu()
+                    # numCorrect = prediction.numpy() == labels.numpy()
+                    # correctInValEpoch += sum(numCorrect)
+                    # top5 = torch.topk(outputs, 5, dim=1)
+                    # top5 = top5[:][1]
+                    # accuracy = [1 if int(labels[ind]) in x else 0 for ind, x in enumerate(top5.numpy())]
+                    # top5InValEpoch += sum(accuracy)
+                    # if batch_num > 5:
+                    #     print('[%d:%.2f] loss: %.3f' % (
+                    #     epoch, batch_num*1.0/num_train_batches,
+                    #     running_loss/output_period
+                    #     ))
+                    #     epoch_train_loss += running_loss
+                    #     running_loss = 0.0
+                    #     gc.collect()
+                    #     print(outputs)
+                    #     _, prediction = torch.max(outputs, dim=1)
+                    #     print('predicted class: ', prediction)
+                    #     print('actual class: ', labels)
+                    #     numCorrect = prediction == labels
+                    #     print('classification error: ', sum(numCorrect)/len(labels))
+                    #     top5 = torch.topk(outputs, 5, dim=1)
+                    #     print('Top 5 classes were: ', top5[:][1])
+                    #     top5 = top5[:][1]
+                    #     # print(labels.repeat(1,5).view(5,-1))
+                    #     print(labels.transpose(0,-1))
+                    #     accuracy = [1 if int(labels[ind]) in x else 0 for ind, x in enumerate(top5.numpy())]
+                    #     print('top5 accuracy: ', sum(accuracy)/len(labels))
+                    #     break
+
+            
+            accuractyString = 'Epoch %d Train: T1  %.2f, T5 %.2f, Loss %.2f \nEpoch %d Val: V1 %.2f, V5 %.2f, Loss %.2f\n' % (
             epoch,
-            100*correctInTrainEpoch/batch_size,
-            100*top5InTrainEpoch/batch_size,
-            100*correctInValEpoch/batch_size,
-            100*top5InValEpoch/batch_size,
+            correctInTrainEpoch/(epoch_samples),
+            top5InTrainEpoch/epoch_samples,
+            epoch_train_loss/(epoch_samples),
+            epoch,
+            correctInValEpoch/(epoch_samples),
+            top5InValEpoch/(epoch_samples),
+            epoch_val_loss/(epoch_samples),
             )
 
             print(accuractyString)
